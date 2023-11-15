e062b97d1f25a2d6109bbb2f4e2c6275c1d2febd
dukekunyura <dukekunyura@gmail.com>
2023-11-15 10:29:30 +0300

    [+] add progress plugin and ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import { sum } from "./test";
 
 console.log("Hey!");
-console.log(sum(3, 7));
-
+console.log(sum(3, 9));
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,38 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+const handler = (percentage, message, ...args) => {
+  // e.g. Output each progress message directly to the console:
+  console.info(percentage, message, ...args);
+};
 
 module.exports = (env) => {
   return {
     mode: env.mode ?? "development",
-    entry: path.resolve(__dirname, "src", "index.js"),
+    entry: path.resolve(__dirname, "src", "index.ts"),
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "public", "index.html"),
+      }),
+      new webpack.ProgressPlugin(handler),
+    ],
     output: {
       path: path.resolve(__dirname, "build"),
       filename: "[name].[contenthash].js",
       clean: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
     },
   };
 };
