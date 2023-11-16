0ae3bb299d3255f7f1f65848989949ba93589774
dukekunyura <dukekunyura@gmail.com>
2023-11-16 15:53:28 +0300

    [+] add devserver

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,11 +1,13 @@
 import path from "path";
 import webpack from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
 
 type Mode = "production" | "development";
 
 interface EnvVariables {
   mode: Mode;
+  port: number;
 }
 
 const handler = (percentage: any, message: any, ...args: any[]) => {
@@ -14,6 +16,8 @@ const handler = (percentage: any, message: any, ...args: any[]) => {
 };
 
 export default (env: EnvVariables) => {
+  const isDev = env.mode === "development";
+
   const config: webpack.Configuration = {
     mode: env.mode ?? "development",
     entry: path.resolve(__dirname, "src", "index.ts"),
@@ -22,7 +26,7 @@ export default (env: EnvVariables) => {
         template: path.resolve(__dirname, "public", "index.html"),
       }),
       new webpack.ProgressPlugin(handler),
-    ],
+    ].filter(Boolean),
     output: {
       path: path.resolve(__dirname, "build"),
       filename: "[name].[contenthash].js",
@@ -40,6 +44,13 @@ export default (env: EnvVariables) => {
     resolve: {
       extensions: [".tsx", ".ts", ".js"],
     },
+    devtool: isDev ? "inline-source-map" : false,
+    devServer: isDev
+      ? {
+          port: env.port ?? 3000,
+          open: true,
+        }
+      : undefined,
   };
   return config;
 };
