4262eb1cb3f3fe135cd41b1ea9bda1054a43cd9f
dukekunyura <dukekunyura@gmail.com>
2023-11-20 01:40:39 +0300

    [*] refactoring config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,76 +1,25 @@
-import path from "path";
 import webpack from "webpack";
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
-
-type Mode = "production" | "development";
+import { buildWebpack } from "./config/build/buildWebpack";
+import { BuildMode, BuildPaths } from "./config/build/types/types";
+import path from "path";
 
 interface EnvVariables {
-  mode: Mode;
-  port: number;
+    mode: BuildMode;
+    port: number;
 }
 
-const handler = (percentage: any, message: any, ...args: any[]) => {
-  // e.g. Output each progress message directly to the console:
-  console.info(percentage, message, ...args);
-};
-
 export default (env: EnvVariables) => {
-  const isDev = env.mode === "development";
-  const isProd = env.mode === "production";
+    const paths: BuildPaths = {
+        output: path.resolve(__dirname, 'build'),
+        entry: path.resolve(__dirname, 'src', 'index.tsx'),
+        html: path.resolve(__dirname, 'public', 'index.html')
+    }
 
+    const config: webpack.Configuration = buildWebpack({
+        port: env.port ?? 3000,
+        mode: env.mode ?? 'development',
+        paths
+    });
 
-  const config: webpack.Configuration = {
-    mode: env.mode ?? "development",
-    entry: path.resolve(__dirname, "src", "index.tsx"),
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, "public", "index.html"),
-      }),
-      isDev && new webpack.ProgressPlugin(handler),
-      isProd && new MiniCssExtractPlugin({
-        // Options similar to the same options in webpackOptions.output
-        // both options are optional
-        filename: "css/[name].[contenthash:8].css",
-        chunkFilename: "css/[name].[contenthash:8].css",
-      }),
-    ].filter(Boolean),
-    output: {
-      path: path.resolve(__dirname, "build"),
-      filename: "[name].[contenthash].js",
-      clean: true,
-    },
-    module: {
-      rules: [
-        {
-          test: /\.s[ac]ss$/i,
-          use: [
-            // Creates `style` nodes from JS strings
-            isDev ? "style-loader" : MiniCssExtractPlugin.loader,
-            // Translates CSS into CommonJS
-            "css-loader",
-            // Compiles Sass to CSS
-            "sass-loader",
-          ],
-        },
-        {
-          test: /\.tsx?$/,
-          use: "ts-loader",
-          exclude: /node_modules/,
-        },
-      ],
-    },
-    resolve: {
-      extensions: [".tsx", ".ts", ".js"],
-    },
-    devtool: isDev ? "inline-source-map" : false,
-    devServer: isDev
-      ? {
-          port: env.port ?? 3000,
-          open: true,
-        }
-      : undefined,
-  };
-  return config;
+    return config;
 };
