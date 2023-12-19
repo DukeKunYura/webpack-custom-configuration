d43d772acf5b9c496b800df2dc0f8cb82efec321
dukekunyura <dukekunyura@gmail.com>
2023-12-20 02:38:22 +0300

    [+] add removeDataTestIdBabelPlugin

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,11 @@
 import { Link, Outlet } from "react-router-dom";
 import classes from "./App.module.scss";
 
-
 function TODO(a: number) {
   console.log(a);
 }
 export const App = () => {
-
-  TODO(1233)
+  TODO(1233);
 
   // if (__PLATFORM__ === "desktop") {
   //   return <div>desktop</div>
@@ -17,9 +15,8 @@ export const App = () => {
   //   return <div>mobile</div>
   // }
 
-
   return (
-    <div>
+    <div data-testid={"App.DataTestId"}>
       <div>PLATFORM={__PLATFORM__}</div>
       <Link to={"/about"}>about</Link>
       <br />
